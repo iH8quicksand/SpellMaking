c423bbe65856cf24fa6b7f8809902a6f77be510a
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-06-05 20:55:10 -0700

    [feat] Spider
    + Spider Spawning & Chasing

--- Assets/Scripts/Levels/EnemySpawner.cs
+++ Assets/Scripts/Levels/EnemySpawner.cs
@@ -228,6 +228,7 @@ public class EnemySpawner : MonoBehaviour
         enemy_prefabs["ant"] = Resources.Load<GameObject>("Ant");
         enemy_prefabs["rat"] = Resources.Load<GameObject>("Rat");
         enemy_prefabs["snake"] = Resources.Load<GameObject>("Snake");
+        enemy_prefabs["spider"] = Resources.Load<GameObject>("Spider");
     }
 
     public void LoadLevelType()
