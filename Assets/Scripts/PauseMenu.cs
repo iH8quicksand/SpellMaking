584128f8bd9cdfe4f2a25646ae6b09e13cb78d1a
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-06-09 02:20:02 -0700

    [feat] Pause Menu
    + Pause Menu Buttons & Input work
    - When hitting pause after game is already running, cursor doesn't exist outside the camera so its impossible to hit the button

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
